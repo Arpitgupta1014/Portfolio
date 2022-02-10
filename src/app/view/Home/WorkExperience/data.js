c04f8e9aa077a4851945eda9166e6f6fa5593515
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-02-11 01:11:33 +0530

    Adding workexperience

--- src/app/view/Home/WorkExperience/data.js
+++ src/app/view/Home/WorkExperience/data.js
@@ -1,5 +1,16 @@
 exports.data = [
 	{
+		tab: "Hacktoberfest",
+		role: "Open-Source Participant",
+		institute: "Hacktoberfest",
+		instituteLink: "https://hacktoberfest.digitalocean.com/",
+		date: "1 October 2021 -31 October 2021",
+		description: [
+			"An event where a participant has to work on a project",
+			"Contributed in open-source projects"
+		],
+	},
+	{
 		tab: "WoC",
 		role: "Open-Source Participant",
 		institute: "Weekend of Code",
@@ -7,7 +18,6 @@ exports.data = [
 		date: "May 2021 - June 2021",
 		description: [
 			"An event where a participant has to work on a project",
-			"Contributed in open-source projects"
 		],
 	},
 
