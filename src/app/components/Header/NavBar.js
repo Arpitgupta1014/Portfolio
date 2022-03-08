487239e8b07ce4323d35cb20f03b90beeea806af
ARPIT GUPTA <ARPITGUPTA1014@gmail.com>
2022-03-08 16:20:58 +0530

    Updateresume

--- src/app/components/Header/NavBar.js
+++ src/app/components/Header/NavBar.js
@@ -131,7 +131,7 @@ const NavBar = () => {
 					data-aos-delay="10"
 					data-aos-duration="1900"
 					data-aos-easing="ease-in-out">
-					<a href="/Arpit_Resume.pdf" target="blank" className={classes.linkNav}>
+					<a href="/ArpitResume.pdf" target="blank" className={classes.linkNav}>
 						Resume
 					</a>
 				</div>
